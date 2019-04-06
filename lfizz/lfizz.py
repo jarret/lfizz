4733c5ed1f9175d4a60b12ac1ee451a1f2cce166
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2019-04-06 21:40:47 +0100

    service

--- lfizz/lfizz.py
+++ lfizz/lfizz.py
@@ -7,17 +7,31 @@ from twisted.application.service import Service
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
 
+import RPi.GPIO as GPIO
+
+LED = 11
+
 
 class LFizz(Service):
     def __init__(self):
         super().__init__()
-        self.count = 0
-        self.count_loop = LoopingCall(self.output_count)
-        self.count_loop.start(1.0, now=False)
+        self.on = False
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(LED, GPIO.OUT)
+        self.led_loop = LoopingCall(self.flip_led)
+        self.led_loop.start(0.5, now=False)
+        print("sadf")
 
-    def output_count(self):
-        print("the count is: %d" % self.count)
-        self.count += 1
+    def flip_led(self):
+        print("sdafdsasadf")
+        if self.on:
+            self.on = False
+            GPIO.output(LED, GPIO.LOW)
+            print("led off")
+        else:
+            self.on = True
+            GPIO.output(LED, GPIO.HIGH)
+            print("led on")
 
     def startService(self):
         super().startService()
@@ -29,4 +43,4 @@ class LFizz(Service):
 
 #if __name__ == '__main__':
 #    lf = LFizz()
-#    lf.run()
+#    reactor.run()
